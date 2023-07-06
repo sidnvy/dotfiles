728f523d0ee87d281f8e03c749d167f885929b7a
Sid <sidnvy@gmail.com>
2023-07-06 12:27:12 -0400

    fix zsh path

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 echo "=========================================================="
 echo "* Install following packages:"
 echo "----------------------------------------------------------"
-echo $PWD
 cwd=$PWD
 cd $HOME
 
 sudo DEBIAN_FRONTEND=noninteractive apt update -yq
+sudo DEBIAN_FRONTEND=noninteractive apt install build-essential zsh tmux ripgrep  fasd -yq
 
 # Find all files in the runcoms directory that do not match README.md and create symbolic links to them in the home directory
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/*; do
@@ -18,13 +18,12 @@ done
 
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-sudo chsh -s /usr/bin/zsh
+sudo chsh -s $(which zsh)
 
 # Install neovim
 NVIM_VERSION=0.9.0
 curl -sL -o $HOME/nvim-linux64.tar.gz https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim-linux64.tar.gz
 sudo tar xzf nvim-linux64.tar.gz --strip-components=1 -C /usr
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq ripgrep tmux
 
 # Install nvchad
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
@@ -35,3 +34,6 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 mv $cwd/nvchad/custom $HOME/.config/nvim/lua
 mv $cwd/.tmux.conf $HOME
 mv $cwd/.zpreztorc $HOME
+
+echo "alias vim=nvim" >> ~/.zshrc
+echo "alias kb=kubectl" >> ~/.zshrc
